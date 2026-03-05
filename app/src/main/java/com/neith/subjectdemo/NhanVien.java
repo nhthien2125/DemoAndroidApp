702b3b8752518fafa777ec8476dc87cf36cf4cb8
BinhTeach <2324801030003@student.tdmu.edu.vn>
2026-03-05 10:06:33 +0700

    Finish

---
+++ app/src/main/java/com/neith/subjectdemo/NhanVien.java
@@ -0,0 +1,13 @@
+package com.neith.subjectdemo;
+
+public class NhanVien {
+    String ma, ho, ten;
+    int namSinh;
+    public NhanVien(){}
+    public NhanVien(String ma, String ho, String ten, int namSinh) {
+        this.ma = ma;
+        this.ho = ho;
+        this.ten = ten;
+        this.namSinh = namSinh;
+    }
+}
